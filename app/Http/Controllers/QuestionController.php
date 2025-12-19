8a90ce3a5bff33a9349a1463c11908de6beddc14
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-12-19 15:22:39 +0700

    upgrade logic for related question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -25,19 +25,40 @@ class QuestionController extends Controller
             'question' => $question,
             'answers' => $answers,
             'sort' => $sort,
-            'relatedQuestions' => $this->getRandomQuestions($question),
+            'relatedQuestions' => $this->getRelatedQuestions($question),
         ]);
     }
-    public function getRandomQuestions($question){
-        if ($question->tags->isEmpty()) {
-            return [];
-        }
-        return cache()->remember("randomQuestions_".$question->id,now()->addMinute(),function() use($question){
+    public function getRelatedQuestions($question){
+        return cache()->remember("relatedQuestions_".$question->id,now()->addMinute(),function() use($question){
+            // if no tags, get latest questions
+            if ($question->tags->isEmpty()) {
+                return $this->getLatestQuestions($question);
+            }
+            // if has tags, get questions with same tags
             $tags=$question->tags->pluck('name');
-            return Question::whereHas('tags',function($query) use($tags){
+            $result = Question::whereHas('tags',function($query) use($tags){
                 $query->whereIn('name',$tags);
-            })->where('id','!=',$question->id)->get()->random(3);
+            })->where('id','!=',$question->id)->inRandomOrder()->limit(3)->get();
+            // if no result, get latest questions
+            if($result->isEmpty()){
+                $result = $this->getLatestQuestions($question);
+            }elseif ($result->count() <3) {
+                $needed = 3 - $result->count();
+                Question::where('id','!=',$question->id)
+                        ->whereNotIn('id',$result->pluck('id')) //exclude already related questions
+                        ->latest()
+                        ->limit($needed)
+                        ->get()
+                        ->merge($result); //merge with existing results
+            }
+            return $result;
         });
+    }
+    public function getLatestQuestions($question){
+        return Question::where('id','!=',$question->id)
+                ->latest()
+                ->limit(3)
+                ->get();
     }
     public function create(){
         return inertia('Questions/Create');
